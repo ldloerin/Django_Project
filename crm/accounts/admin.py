bbad234140feca13414580e29f45c8b2b995ac15
David Loerinci <david.loerinci@gmail.com>
2021-09-13 11:42:31 +0200

    clean code

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import *
+from .models import Customer
+from .models import Order
+from .models import Product
 
 # Register your models here.
 
 admin.site.register(Customer)
-admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Product)
