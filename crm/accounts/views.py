441b68c11bca73b1bd2a80d99aea8e1fe54fb705
David Loerinci <david.loerinci@gmail.com>
2021-09-14 11:38:06 +0200

    rendering data to templates

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
+from .models import Customer
+from .models import Order
+from .models import Product
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+    'customers':customers,
+    'orders':orders,
+    'total_orders':total_orders,
+    'delivered':delivered,
+    'pending':pending
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all().order_by('name')
+
+    return render(request, 'accounts/products.html', {'products':products})
 
 
 def customers(request):
