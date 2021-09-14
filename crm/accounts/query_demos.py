920d2d40e06395a2d5ce702c961c3e1e5a045b31
David Loerinci <david.loerinci@gmail.com>
2021-09-14 11:56:54 +0200

    clean code

--- crm/accounts/query_demos.py
+++ crm/accounts/query_demos.py
@@ -30,7 +30,7 @@ products = Product.objects.filter(category="Out Door")
 
 # (9)***Order/Sort Objects by id
 leastToGreatest = Product.objects.all().order_by('id')
-greatestToLeast = Product.objects.all().order_by('-id') 
+greatestToLeast = Product.objects.all().order_by('-id')
 
 
 # (10) Returns all products with tag of "Sports": (Query Many to Many Fields)
@@ -39,7 +39,7 @@ productsFiltered = Product.objects.filter(tags__name="Sports")
 '''
 (11)Bonus
 Q: If the customer has more than 1 ball, how would you reflect it in the database?
-A: Because there are many different products and this value changes constantly you would most 
+A: Because there are many different products and this value changes constantly you would most
 likly not want to store the value in the database but rather just make this a function we can run
 each time we load the customers profile
 '''
@@ -63,9 +63,11 @@ for order in firstCustomer.order_set.all():
 class ParentModel(models.Model):
     name = models.CharField(max_length=200, null=True)
 
+
 class ChildModel(models.Model):
     parent = models.ForeignKey(Customer)
     name = models.CharField(max_length=200, null=True)
+
 
 parent = ParentModel.objects.first()
 # Returns all child models related to parent
