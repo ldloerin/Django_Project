b9edc9e1b3adc6ce74a4fe23f075096f32f2aa25
David Loerinci <david.loerinci@gmail.com>
2021-09-14 15:17:03 +0200

    added customer link buttons

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/<str:pk_test>/', views.customers),
+    path('', views.home, name='home_url_name'),
+    path('products/', views.products, name='product_url_name'),
+    path('customers/<str:pk_test>/', views.customers, name='customer_url_name'),
 ]
