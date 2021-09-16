7539f7cadc123c801d710732eaa839acbe1d94ba
David Loerinci <david.loerinci@gmail.com>
2021-09-16 14:35:47 +0200

    clean code

--- crm/accounts/filters.py
+++ crm/accounts/filters.py
@@ -8,7 +8,7 @@ class OrderFilter(FilterSet):
     start_date = DateFilter(field_name="date_created", lookup_expr='gte')
     end_date = DateFilter(field_name="date_created", lookup_expr='lte')
     note = CharFilter(field_name="note", lookup_expr='icontains')
-    
+
     class Meta:
         model = Order
         fields = '__all__'
